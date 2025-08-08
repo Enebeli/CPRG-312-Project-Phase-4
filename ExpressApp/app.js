2d125b3e225262ae05e774ad22c848b5dd11f2e0
prashsharma88 <prashant.sharma@sait.ca>
2025-08-08 10:14:05 -0600

    removed unused files and updated DB connection string to use value from env file

--- ExpressApp/app.js
+++ ExpressApp/app.js
@@ -25,7 +25,9 @@ app.use('/comment', commentRouter);
 
 const server = http.createServer(app);
 
-mongoose.connect("mongodb://localhost:27017/abac_lab")
+console.log(`${process.env.DB_URL}/${process.env.DATABASE_NAME}`)
+
+mongoose.connect(`${process.env.DB_URL}/${process.env.DATABASE_NAME}`)
 .then(() => {
     server.listen(3000, () => {
         console.log("server started at port 3000");
